160a3babf11a2a80a856132475bf1d1f9c2e7896
Iman Tabrizian <itabrizian@nvidia.com>
2020-11-08 18:40:30 -0500

    Remove NVML from gpu_device_factory

--- model_analyzer/device/gpu_device_factory.py
+++ model_analyzer/device/gpu_device_factory.py
@@ -23,19 +23,20 @@
 # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import numba.cuda
-from pynvml import nvmlInit, nvmlShutdown, nvmlDeviceGetHandleByPciBusId, \
-    nvmlDeviceGetUUID, nvmlDeviceGetPciInfo, nvmlDeviceGetIndex
 from model_analyzer.device.gpu_device import GPUDevice
+import model_analyzer.monitor.dcgm.dcgm_agent as dcgm_agent
+import model_analyzer.monitor.dcgm.dcgm_structs as structs
+from model_analyzer.model_analyzer_exceptions import TritonModelAnalyzerException
 
 
 class GPUDeviceFactory:
     """
     Factory class for creating GPUDevices
     """
-
     @staticmethod
-    def create_device_by_bus_id(bus_id):
+    def create_device_by_bus_id(bus_id, dcgmPath=None):
         """
         Create a GPU device by using its bus ID.
 
@@ -52,14 +53,29 @@ class GPUDeviceFactory:
         Device
             The device associated with this bus id.
         """
-        nvmlInit()
-        handle = nvmlDeviceGetHandleByPciBusId(bus_id)
-        device_uuid = nvmlDeviceGetUUID(handle)
-        device_id = nvmlDeviceGetIndex(handle)
-        nvmlShutdown()
 
-        gpu_device = GPUDevice(device_id, bus_id, device_uuid)
-        return gpu_device
+        structs._dcgmInit(dcgmPath)
+        dcgm_agent.dcgmInit()
+
+        # Start DCGM in the embedded mode to use the shared library
+        dcgm_handle = dcgm_agent.dcgmStartEmbedded(
+            structs.DCGM_OPERATION_MODE_MANUAL)
+        gpu_devices = dcgm_agent.dcgmGetAllSupportedDevices(dcgm_handle)
+        for gpu_device in gpu_devices:
+            device_atrributes = dcgm_agent.dcgmGetDeviceAttributes(
+                dcgm_handle, gpu_device).identifiers
+            pci_bus_id = bytes(
+                device_atrributes.pciBusId.decode('ascii').upper(),
+                encoding='ascii')
+            device_uuid = device_atrributes.uuid
+            if pci_bus_id == bus_id:
+                gpu_device = GPUDevice(gpu_device, bus_id, device_uuid)
+                dcgm_agent.dcgmShutdown()
+                return gpu_device
+        else:
+            dcgm_agent.dcgmShutdown()
+            raise TritonModelAnalyzerException(
+                f'GPU with {bus_id} bus id is not supported by DCGM.')
 
     @staticmethod
     def create_device_by_cuda_index(index):
@@ -82,16 +98,17 @@ class GPUDeviceFactory:
         IndexError
             If the index is out of bound.
         """
+
         devices = numba.cuda.list_devices()
         if index > len(devices) - 1:
             raise IndexError
 
         cuda_device = devices[index]
         device_identity = cuda_device.get_device_identity()
-        device_bus_id = '{:02x}:{:02x}:{:02x}'.format(
-            device_identity['pci_domain_id'],
-            device_identity['pci_bus_id'],
-            device_identity['pci_device_id'])
+        pci_domain_id = device_identity['pci_domain_id']
+        pci_device_id = device_identity['pci_device_id']
+        pci_bus_id = device_identity['pci_bus_id']
+        device_bus_id = f'{pci_domain_id:08X}:{pci_bus_id:02X}:{pci_device_id:02X}.0'
 
         return GPUDeviceFactory.create_device_by_bus_id(
             bytes(device_bus_id, encoding='ascii'))
