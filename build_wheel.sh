15ea80080168f526900355233799e8192e953ca8
Ashwin Ramesh <asramesh@nvidia.com>
2020-12-01 09:45:26 -0800

    Build model-analyzer as a manylinux1_x86_64.whl (#42)

---
+++ build_wheel.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Copyright 2020, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PYTHON=${PYTHON:=`which python3`}
+
+if [[ $# -lt 2 ]] ; then
+    echo "usage: $0 <perf-analyzer-binary> <linux-specific>"
+    exit 1
+fi
+
+if [[ ! -f "VERSION" ]]; then
+    echo "Could not find VERSION"
+    exit 1
+fi
+
+if [[ ! -f "LICENSE" ]]; then
+    echo "Could not find LICENSE"
+    exit 1
+fi
+
+if [[ ! -f "$1" ]]; then
+    echo "Could not find perf_analyzer binary"
+    exit 1
+fi
+
+WHLDIR="`pwd`/wheels"
+mkdir -p ${WHLDIR}
+
+# Copy required files into WHEELDIR temporarily
+cp $1 "${WHLDIR}"
+cp VERSION "${WHLDIR}"
+cp LICENSE "${WHLDIR}"
+cp requirements.txt "${WHLDIR}"
+
+# Set platform and build wheel
+echo $(date) : "=== Building wheel"
+if [ "$2" = true ] ; then
+    PLATFORM=`uname -m`
+    if [ "$PLATFORM" = "aarch64" ] ; then
+        PLATFORM_NAME="linux_aarch64"
+    else
+        PLATFORM_NAME="manylinux1_x86_64"
+    fi
+    ${PYTHON} setup.py  bdist_wheel --plat-name $PLATFORM_NAME --dependency-dir $WHLDIR
+else
+    ${PYTHON} setup.py bdist_wheel --dependency-dir $WHLDIR
+fi
+rm -f $WHLDIR/* && cp dist/* $WHLDIR
+rm -rf build dist model_analyzer.egg-info
+touch ${WHLDIR}/stamp.whl
+echo $(date) : "=== Output wheel file is in: ${WHLDIR}"
