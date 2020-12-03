28802bd97d0397e3809ffca1fd69df7e7273f383
Iman Tabrizian <itabrizian@nvidia.com>
2020-12-02 19:45:00 -0500

    Add automatic port allocation (#47)

--- qa/L0_analyze/test.sh
+++ qa/L0_analyze/test.sh
@@ -32,10 +32,13 @@ TRITON_LAUNCH_MODE="docker"
 TRITON_SERVER_VERSION="20.11-py3"
 CLIENT_PROTOCOL="grpc"
 TEST_OUTPUT_NUM_COLUMNS=7
+PORTS=(`find_available_ports 3`)
 
 MODEL_ANALYZER_ARGS="-m $MODEL_REPOSITORY -n $MODEL_NAMES -b $BATCH_SIZES -c $CONCURRENCY"
 MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_ARGS --client-protocol=$CLIENT_PROTOCOL --triton-launch-mode=$TRITON_LAUNCH_MODE --triton-version=$TRITON_SERVER_VERSION"
 MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_ARGS --export -e $EXPORT_PATH --filename-server-only=$FILENAME_SERVER_ONLY --filename-model=$FILENAME_MODEL"
+MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_ARGS --triton-http-endpoint localhost:${PORTS[0]} --triton-grpc-endpoint localhost:${PORTS[1]}"
+MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_ARGS --triton-metrics-url http://localhost:${PORTS[2]}/metrics"
 
 # Compute expected columns
 NUM_MODELS=`echo $QA_MODELS | awk '{print NF}'`
