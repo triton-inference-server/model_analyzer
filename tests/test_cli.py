6c09d26e4e1455d846fcecc0003610c08b1919d1
Matthew Kotila <matthew.r.kotila@gmail.com>
2021-11-13 00:55:12 +0000

    addressed comments

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,44 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest.mock import patch
 import sys
-
-from argparse import ArgumentParser
 
 from .common import test_result_collector as trc
 
-from model_analyzer.config.input.config_command_profile import ConfigCommandProfile
-from model_analyzer.config.input.config_command_analyze import ConfigCommandAnalyze
-from model_analyzer.config.input.config_command_report import ConfigCommandReport
 from model_analyzer.cli.cli import CLI
-
-
-class ArgumentParserSubclass(ArgumentParser):
-    """
-    Custom subclass of the ArgumentParser class to change functionality to enable
-    testing
-    """
-
-    def print_help(self, file=None):
-        super().print_help(file)
-        self._did_print_help = True
-
-    _did_print_help = False
-
-
-class CLISubclass(CLI):
-    """
-    Custom subclass of the CLI class to change functionality to enable testing
-    """
-
-    def __init__(self):
-        self._parser = ArgumentParserSubclass()
-        self._add_global_options()
-        self._subparsers = self._parser.add_subparsers(
-            help='Subcommands under Model Analyzer', dest='subcommand')
-
-        # Store subcommands, and their configs
-        self._subcommand_configs = {}
 
 
 class TestCLI(trc.TestResultCollector):
@@ -57,7 +25,8 @@ class TestCLI(trc.TestResultCollector):
     Tests the methods of the CLI class
     """
 
-    def test_help_message_no_args(self):
+    @patch('model_analyzer.cli.cli.ArgumentParser.print_help')
+    def test_help_message_no_args(self, mock_print_help):
         """
         Tests that model-analyzer prints the help message when no arguments are
         given
@@ -65,24 +34,7 @@ class TestCLI(trc.TestResultCollector):
 
         sys.argv = ['/usr/local/bin/model-analyzer']
 
-        config_profile = ConfigCommandProfile()
-        config_analyze = ConfigCommandAnalyze()
-        config_report = ConfigCommandReport()
-
-        cli = CLISubclass()
-        cli.add_subcommand(
-            cmd='profile',
-            help=
-            'Run model inference profiling based on specified CLI or config options.',
-            config=config_profile)
-        cli.add_subcommand(
-            cmd='analyze',
-            help=
-            'Collect and sort profiling results and generate data and summaries.',
-            config=config_analyze)
-        cli.add_subcommand(cmd='report',
-                           help='Generate detailed reports for a single config',
-                           config=config_report)
+        cli = CLI()
 
         self.assertRaises(SystemExit, cli.parse)
-        self.assertTrue(cli._parser._did_print_help == True)
+        mock_print_help.assert_called()
