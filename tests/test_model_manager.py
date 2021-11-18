18f7098b6fdb670b3efd4a84f3a86481fdc02d64
Elias Bermudez <de.bermudez13@gmail.com>
2021-11-17 16:04:48 -0800

    Format test model manager file

--- tests/test_model_manager.py
+++ tests/test_model_manager.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from .common import test_result_collector as trc
 
 from .mocks.mock_config import MockConfig
@@ -36,17 +35,18 @@ from unittest.mock import MagicMock
 from unittest.mock import patch
 
 
-
 class ModelManagerSubclass(ModelManager):
     """ 
     Overrides execute_run_configs() to gather a list of tuples that contain 
     the main values of each 'executed' run_config
     """
 
-    def __init__(self, config, client, server, metrics_manager, result_manager, state_manager):
-        super().__init__(config, client, server, metrics_manager, result_manager, state_manager)
+    def __init__(self, config, client, server, metrics_manager, result_manager,
+                 state_manager):
+        super().__init__(config, client, server, metrics_manager,
+                         result_manager, state_manager)
         self._configs = []
-    
+
     def _execute_run_configs(self):
         while self._run_config_generator.run_configs():
             config = self._run_config_generator.next_config()
@@ -59,18 +59,21 @@ class ModelManagerSubclass(ModelManager):
 
             dynamic_batching = None
             if model_config.get("dynamicBatching") is not None:
-                dynamic_batching = model_config["dynamicBatching"].get("preferredBatchSize", [0])[0]
+                dynamic_batching = model_config["dynamicBatching"].get(
+                    "preferredBatchSize", [0])[0]
 
             batch_size = perf_config.__getitem__("batch-size")
             concurrency = perf_config.__getitem__("concurrency-range")
 
-            self._configs.append((instances, dynamic_batching, batch_size, concurrency))
+            self._configs.append(
+                (instances, dynamic_batching, batch_size, concurrency))
 
     def get_run_configs(self):
         return self._configs
 
 
-@patch('model_analyzer.config.run.run_search.RunSearch.add_measurements', MagicMock())
+@patch('model_analyzer.config.run.run_search.RunSearch.add_measurements',
+       MagicMock())
 class TestModelManager(trc.TestResultCollector):
 
     def test_full_sweep(self):
@@ -78,10 +81,10 @@ class TestModelManager(trc.TestResultCollector):
         Test a normal full sweep of options
         """
 
-        expected_instances = [1,2,3,4,5]
-        expected_batching = [None,0,1,2,4,8,16]
+        expected_instances = [1, 2, 3, 4, 5]
+        expected_batching = [None, 0, 1, 2, 4, 8, 16]
         expected_batch_sizes = [1]
-        expected_concurrency = [1,2,4,8,16,32,64,128]
+        expected_concurrency = [1, 2, 4, 8, 16, 32, 64, 128]
 
         yaml_content = """
             run_config_search_max_concurrency: 128
@@ -89,21 +92,23 @@ class TestModelManager(trc.TestResultCollector):
             run_config_search_max_instance_count: 5
             run_config_search_preferred_batch_size_disable : False
             run_config_search_disable: False
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
 
     def test_another_full_sweep(self):
         """
         Test another full sweep of options
         """
 
-        expected_instances = [1,2,3,4,5,6,7]
-        expected_batching = [None,0,1,2,4,8]
+        expected_instances = [1, 2, 3, 4, 5, 6, 7]
+        expected_batching = [None, 0, 1, 2, 4, 8]
         expected_batch_sizes = [1]
-        expected_concurrency = [1,2,4,8,16,32]
+        expected_concurrency = [1, 2, 4, 8, 16, 32]
 
         yaml_content = """
             run_config_search_max_concurrency: 32
@@ -111,21 +116,23 @@ class TestModelManager(trc.TestResultCollector):
             run_config_search_max_instance_count: 7
             run_config_search_preferred_batch_size_disable : False
             run_config_search_disable: False
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
 
     def test_preferred_batch_size_disable(self):
         """
         Test with search_preferred_batch_size_disable=True
         """
 
-        expected_instances = [1,2,3,4,5,6,7]
+        expected_instances = [1, 2, 3, 4, 5, 6, 7]
         expected_batching = [None]
         expected_batch_sizes = [1]
-        expected_concurrency = [1,2,4,8,16,32]
+        expected_concurrency = [1, 2, 4, 8, 16, 32]
 
         yaml_content = """
             run_config_search_max_concurrency: 32
@@ -133,11 +140,13 @@ class TestModelManager(trc.TestResultCollector):
             run_config_search_max_instance_count: 7
             run_config_search_preferred_batch_size_disable : True
             run_config_search_disable: False
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
 
     def test_run_search_disable(self):
         """
@@ -157,22 +166,23 @@ class TestModelManager(trc.TestResultCollector):
             run_config_search_max_instance_count: 7
             run_config_search_preferred_batch_size_disable : False
             run_config_search_disable: True
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
 
     def test_manual_concurrency(self):
         """
         Test with manually specified concurrencies
         """
 
-        expected_instances = [1,2,3,4,5,6,7]
-        expected_batching = [None,0,1,2,4,8]
+        expected_instances = [1, 2, 3, 4, 5, 6, 7]
+        expected_batching = [None, 0, 1, 2, 4, 8]
         expected_batch_sizes = [1]
-        expected_concurrency = [5,7]
-
+        expected_concurrency = [5, 7]
 
         yaml_content = """
             run_config_search_max_concurrency: 32
@@ -181,11 +191,13 @@ class TestModelManager(trc.TestResultCollector):
             run_config_search_preferred_batch_size_disable : False
             run_config_search_disable: False
             concurrency: [5, 7]
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
 
     def test_remote_mode(self):
         """
@@ -197,7 +209,7 @@ class TestModelManager(trc.TestResultCollector):
         expected_instances = [None]
         expected_batching = [None]
         expected_batch_sizes = [1]
-        expected_concurrency = [1,2,4,8,16,32,64,128,256,512]
+        expected_concurrency = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
         yaml_content = """
             run_config_search_max_concurrency: 512
@@ -206,22 +218,23 @@ class TestModelManager(trc.TestResultCollector):
             run_config_search_preferred_batch_size_disable : False
             run_config_search_disable: False
             triton_launch_mode: remote            
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
-
 
     def test_manual_parameters(self):
         """
         Test with manually specified concurrencies and batch sizes
         """
 
-        expected_instances = [1,2,3,4,5,6,7]
+        expected_instances = [1, 2, 3, 4, 5, 6, 7]
         expected_batching = [None, 0, 1, 2, 4, 8]
-        expected_batch_sizes = [1,2,3]
-        expected_concurrency = [2,10,18,26,34,42,50,58]
+        expected_batch_sizes = [1, 2, 3]
+        expected_concurrency = [2, 10, 18, 26, 34, 42, 50, 58]
 
         yaml_content = """
             run_config_search_max_concurrency: 512
@@ -234,11 +247,13 @@ class TestModelManager(trc.TestResultCollector):
                 stop: 64
                 step: 8
             batch_sizes: 1,2,3          
-            """   
+            """
 
-        expected_values = [expected_instances, expected_batching, expected_batch_sizes, expected_concurrency]
+        expected_values = [
+            expected_instances, expected_batching, expected_batch_sizes,
+            expected_concurrency
+        ]
         self._test_model_manager(yaml_content, expected_values)
-
 
     def _test_model_manager(self, yaml_content, expected_values):
         """ 
@@ -254,19 +269,20 @@ class TestModelManager(trc.TestResultCollector):
 
         config = self._evaluate_config(args, yaml_content)
         state_manager = AnalyzerStateManager(config, MagicMock())
-        model_manager = ModelManagerSubclass(config, MagicMock(), MagicMock(), MagicMock(), MagicMock(), state_manager)
+        model_manager = ModelManagerSubclass(config, MagicMock(), MagicMock(),
+                                             MagicMock(), MagicMock(),
+                                             state_manager)
 
         model_manager.run_model(config.profile_models[0])
 
         run_configs = model_manager.get_run_configs()
         run_configs_set = set(run_configs)
-        
+
         expected_set = set(itertools.product(*expected_values))
 
         # Confirm that the full set of configs is exactly the same as expected
         #
         self.assertEqual(expected_set, run_configs_set)
-
 
     def _evaluate_config(self, args, yaml_content):
         """ Parse the given yaml_content into a config and return it """
@@ -282,7 +298,7 @@ class TestModelManager(trc.TestResultCollector):
             config=config)
         cli.parse()
         mock_config.stop()
-        return config    
+        return config
 
     def setUp(self):
         # Use mock model config or else TritonModelAnalyzerException will be thrown as it tries to read from disc
@@ -291,4 +307,3 @@ class TestModelManager(trc.TestResultCollector):
 
     def tearDown(self):
         self.mock_model_config.stop()
-
