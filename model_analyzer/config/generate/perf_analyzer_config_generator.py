5847de9ed638589fc2be0c5f7672d343af52a7ee
tgerdes <tgerdes@nvidia.com>
2022-01-14 10:33:19 -0600

    Make RCG be state machine. Add in early exit to PACG

--- model_analyzer/config/generate/perf_analyzer_config_generator.py
+++ model_analyzer/config/generate/perf_analyzer_config_generator.py
@@ -15,12 +15,18 @@
 from .config_generator_interface import ConfigGeneratorInterface
 from .generator_utils import GeneratorUtils as utils
 
-from model_analyzer.perf_analyzer.perf_config import PerfAnalyzerConfig
 from model_analyzer.config.input.config_defaults import DEFAULT_MEASUREMENT_MODE
+from model_analyzer.constants import THROUGHPUT_GAIN
+from model_analyzer.perf_analyzer.perf_config import PerfAnalyzerConfig
 
 
 class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
-    """ Given Perf Analyzer configuration options, generates Perf Analyzer configs """
+    """ 
+    Given Perf Analyzer configuration options, generates Perf Analyzer configs 
+    
+    All combinations are created at time 0, but it may return is_done==true 
+    earlier depending on results that it receives
+    """
 
     def __init__(self, cli_config, model_name, model_perf_analyzer_flags):
         """
@@ -32,6 +38,9 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
         model_name: str
             Name of the model
         """
+        self._curr_config_index = 0
+        self._last_results = ["valid"]
+        self._all_results = []
 
         self._model_name = model_name
         self._perf_analyzer_flags = model_perf_analyzer_flags
@@ -51,11 +60,14 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
 
     def is_done(self):
         """ Returns true if this generator is done generating configs """
-        return len(self._configs) == 0
+        return self._all_configs_returned() or self._last_results_erroneous(
+        ) or not self._throughput_gain_valid()
 
     def next_config(self):
         """ Returns the next generated config """
-        return self._configs.pop(0)
+        config = self._configs[self._curr_config_index]
+        self._curr_config_index += 1
+        return config
 
     def set_last_results(self, measurement):
         """ 
@@ -66,7 +78,8 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
         ----------
         measurement: Measurement from the last run
         """
-        pass
+        self._last_results = measurement
+        self._all_results.extend(measurement)
 
     def _create_concurrency_list(self, cli_config):
         if cli_config.concurrency:
@@ -114,3 +127,27 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
             })
 
         return perf_config_params
+
+    def _all_configs_returned(self):
+        return len(self._configs) == self._curr_config_index
+
+    def _last_results_erroneous(self):
+        return not self._last_results
+
+    def _throughput_gain_valid(self):
+        if len(self._all_results) < 4:
+            return True
+
+        return self._calculate_throughput_gain(1) > THROUGHPUT_GAIN or \
+            self._calculate_throughput_gain(2) > THROUGHPUT_GAIN or \
+            self._calculate_throughput_gain(3) > THROUGHPUT_GAIN
+
+    def _calculate_throughput_gain(self, index):
+        throughput_before = self._get_throughput(
+            self._all_results[-(index + 1)])
+        throughput_after = self._get_throughput(self._all_results[-index])
+        gain = (throughput_after - throughput_before) / throughput_before
+        return gain
+
+    def _get_throughput(self, measurement):
+        return measurement.get_metric_value('perf_throughput')
