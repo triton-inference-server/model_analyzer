79d1d6b59b6383381d570c4fcebd606e8334adf6
Brian Raf <braf@nvidia.com>
2022-03-08 00:21:46 +0000

    Initial design of new class with TDD complete

---
+++ model_analyzer/result/results.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from model_analyzer.constants import LOGGER_NAME
+
+import logging
+
+logger = logging.getLogger(LOGGER_NAME)
+
+
+class Results:
+    """
+    Provides storage and accessor functions for measurements
+    """
+    MODEL_CONFIG_INDEX = 0
+    MEASUREMENTS_INDEX = 1
+
+    def __init__(self):
+        """          
+        """
+        self._results = {}
+        self._done = {}
+
+    @classmethod
+    def from_dict(cls, results_dict):
+        results = Results()
+        results._results = results_dict['_results']
+
+        return results
+
+    def add_measurement(self, run_config, key, measurement):
+        """
+        Given a RunConfig and a key store a measurement
+        
+        Parameters
+        ----------
+        run_config: RunConfig
+        key: str
+        measurement: Measurement
+        """
+        model_name, model_config, model_config_name = self._extract_run_config_fields(
+            run_config)
+
+        if model_name not in self._results:
+            self._results[model_name] = {}
+            self._done[model_name] = False
+
+        if model_config_name not in self._results[model_name]:
+            self._results[model_name][model_config_name] = (model_config, {})
+
+        self._results[model_name][model_config_name][
+            Results.MEASUREMENTS_INDEX][key] = measurement
+
+    def is_done(self, model_name):
+        return self._done[model_name]
+
+    def next_result(self, model_name):
+        """
+        Given a model name creates a generator that returns
+        model configs with their corresponding measurements
+        
+        Parameters
+        ----------
+        model_name : str
+            The model name for the requested results
+
+        Returns
+        -------
+        Tuple
+            Tuple consisting of the model_config_name and 
+            it's corresponding measurements
+        """
+        if model_name not in self._results:
+            logger.warning(
+                f"Model {model_name} requested for analysis but no results were found. "
+                "Ensure that this model was actually profiled.")
+            return None
+
+        results_list = list(self._results[model_name].values())
+
+        self._done[model_name] = False
+        for result in results_list:
+            if result == results_list[-1]:
+                self._done[model_name] = True
+
+            yield result[Results.MODEL_CONFIG_INDEX], result[
+                Results.MEASUREMENTS_INDEX]
+
+    def get_model_config_measurements(self, model_name, model_config_name):
+        """
+        Given a model name and model config name returns the model config and 
+        a list of all associated measurement values
+        
+        Parameters
+        ----------
+        model_name : str
+            The model name for the requested results
+            
+        model_config_name: str
+            The model config name for the requested results
+
+        Returns
+        -------
+        Tuple
+            Tuple consisting of the model_config and a list of
+            all associated measurement values
+        """
+        if model_name not in self._results or model_config_name not in self._results[
+                model_name]:
+            logger.error(
+                f'No results found for model config: {model_config_name}')
+            return (None, [])
+
+        model_config_data = self._results[model_name][model_config_name]
+
+        return model_config_data[Results.MODEL_CONFIG_INDEX], list(
+            model_config_data[Results.MEASUREMENTS_INDEX].values())
+
+    def _extract_run_config_fields(self, run_config):
+        return (run_config.model_name(), run_config.model_configs()[0],
+                run_config.model_configs()[0].get_field('name'))
---
+++ tests/test_results.py
@@ -0,0 +1,114 @@
+# Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from model_analyzer.result.results import Results
+from model_analyzer.triton.model.model_config import ModelConfig
+from model_analyzer.config.run.run_config import RunConfig
+
+import unittest
+from unittest.mock import MagicMock, patch
+from .common import test_result_collector as trc
+
+
+class TestResults(trc.TestResultCollector):
+
+    def setUp(self):
+        self._construct_results()
+
+    def tearDown(self):
+        NotImplemented
+
+    def _construct_results(self):
+        self._result = Results()
+
+        self._run_config = []
+        self._run_config.append(
+            self._create_run_config('modelA', 'model_config_0'))
+
+        self._run_config.append(
+            self._create_run_config('modelA', 'model_config_1'))
+
+        self._run_config.append(
+            self._create_run_config('modelA', 'model_config_2'))
+
+        self._measurements = []
+        self._measurements.append({"key_A": "1", "key_B": "2", "key_C": "3"})
+        self._measurements.append({"key_D": "4", "key_E": "5", "key_F": "6"})
+        self._measurements.append({"key_G": "7", "key_H": "8", "key_I": "9"})
+
+        self._result.add_measurement(self._run_config[0], "key_A", "1")
+        self._result.add_measurement(self._run_config[0], "key_B", "2")
+        self._result.add_measurement(self._run_config[0], "key_C", "3")
+
+        self._result.add_measurement(self._run_config[1], "key_D", "4")
+        self._result.add_measurement(self._run_config[1], "key_E", "5")
+        self._result.add_measurement(self._run_config[1], "key_F", "6")
+
+        self._result.add_measurement(self._run_config[2], "key_G", "7")
+        self._result.add_measurement(self._run_config[2], "key_H", "8")
+        self._result.add_measurement(self._run_config[2], "key_I", "9")
+
+        run_config_0 = self._create_run_config('modelB', 'model_config_0')
+        self._result.add_measurement(run_config_0, "key_F", "6")
+        self._result.add_measurement(run_config_0, "key_E", "5")
+        self._result.add_measurement(run_config_0, "key_D", "4")
+
+        run_config_1 = self._create_run_config('modelB', 'model_config_1')
+        self._result.add_measurement(run_config_1, "key_C", "3")
+        self._result.add_measurement(run_config_1, "key_B", "2")
+        self._result.add_measurement(run_config_1, "key_A", "1")
+
+    def test_is_done(self):
+        resultA = self._result.next_result('modelA')
+        resultB = self._result.next_result('modelB')
+
+        modelA_config_cnt = 0
+        while not self._result.is_done('modelA'):
+            foo = next(resultA)
+            modelA_config_cnt = modelA_config_cnt + 1
+
+        modelB_config_cnt = 0
+        while not self._result.is_done('modelB'):
+            boo = next(resultB)
+            modelB_config_cnt = modelB_config_cnt + 1
+
+        self.assertEqual(modelA_config_cnt, 3)
+        self.assertEqual(modelB_config_cnt, 2)
+
+    def test_next_result(self):
+        result = self._result.next_result('modelA')
+
+        for index, run_config in enumerate(self._run_config):
+            model_config, measurements = next(result)
+
+            self.assertEqual(model_config, run_config.model_configs()[0])
+            self.assertEqual(measurements, self._measurements[index])
+
+    def test_get_model_config_measurements(self):
+        model_config, measurements = self._result.get_model_config_measurements(
+            'modelA', 'model_config_1')
+
+        self.assertEqual(model_config, self._run_config[1].model_configs()[0])
+        self.assertEqual(measurements, list(self._measurements[1].values()))
+
+    def _create_run_config(self, model_name, model_config_name):
+        model_config_dict = {'name': model_config_name}
+        self._model_config = ModelConfig.create_from_dictionary(
+            model_config_dict)
+
+        return RunConfig(model_name, [self._model_config], None, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
