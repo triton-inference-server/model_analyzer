1404494847c5e981349b42735c0c3a02a10a708f
nv-hwoo <hwoo@nvidia.com>
2022-06-22 10:28:40 -0700

    Clean up unused variable

--- model_analyzer/config/generate/perf_analyzer_config_generator.py
+++ model_analyzer/config/generate/perf_analyzer_config_generator.py
@@ -65,10 +65,6 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
         self._configs = []
         self._concurrency_warning_printed = False
 
-        # Flag to indicate we have started to return results
-        #
-        self._generator_started = False
-
         self._last_results = ["valid"]
         self._all_results = []
 
@@ -98,7 +94,6 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
 
     def get_configs(self):
         """ Returns the next generated config """
-        self._generator_started = True
         while True:
             if self._is_done():
                 break
@@ -202,8 +197,7 @@ class PerfAnalyzerConfigGenerator(ConfigGeneratorInterface):
         self._curr_concurrency_index += 1
 
     def _done_walking(self):
-        return self._generator_started \
-           and self._done_walking_configs() \
+        return self._done_walking_configs() \
            and self._done_walking_concurrencies()
 
     def _done_walking_configs(self):
