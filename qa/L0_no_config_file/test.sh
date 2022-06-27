dd9cfa1bf703ebae2f3b4342cc6bc85b075fb6bb
tgerdes <tgerdes@nvidia.com>
2022-06-27 10:15:29 -0500

    New L0 test for no config file

---
+++ qa/L0_no_config_file/test.sh
@@ -0,0 +1,184 @@
+# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+###############################
+# Test that if we run model_analyzer on a model with no config.pbtxt,
+# that we can generate a default model config (in some cases)
+#
+# We test this by copying some models locally into a temp model repository
+# and deleting the config files before running
+#
+# Only local and docker modes are tested and are expected to work
+# - Remote mode is excluded because the model must already be loaded before
+#   calling model analyzer in remote mode
+# - c_api mode is excluded because perf_analyzer is our client for c_api mode
+#   and does not support this functionality
+###############################
+
+source ../common/util.sh
+
+rm -f *.log
+OUTPUT_MODEL_REPOSITORY=${OUTPUT_MODEL_REPOSITORY:=`get_output_directory`}
+rm -rf $OUTPUT_MODEL_REPOSITORY
+
+MODEL_REPOSITORY=$(get_output_directory)
+rm -rf $MODEL_REPOSITORY
+mkdir $MODEL_REPOSITORY
+cp -R /mnt/nvdl/datasets/inferenceserver/$REPO_VERSION/tf_model_store/resnet_v1_50_cpu_savedmodel $MODEL_REPOSITORY
+cp -R /mnt/nvdl/datasets/inferenceserver/$REPO_VERSION/libtorch_model_store/vgg19_libtorch $MODEL_REPOSITORY
+rm $MODEL_REPOSITORY/*/config.pbtxt
+
+# Set test parameters
+MODEL_ANALYZER="`which model-analyzer`"
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+TRITON_DOCKER_IMAGE=${TRITONSERVER_BASE_IMAGE_NAME}
+TRITON_LAUNCH_MODES="local docker"
+CLIENT_PROTOCOLS="http grpc"
+PORTS=(`find_available_ports 3`)
+http_port="${PORTS[0]}"
+grpc_port="${PORTS[1]}"
+metrics_port="${PORTS[2]}"
+MODEL_ANALYZER_BASE_ARGS="-m $MODEL_REPOSITORY --override-output-model-repository"
+MODEL_ANALYZER_BASE_ARGS="$MODEL_ANALYZER_BASE_ARGS --output-model-repository-path $OUTPUT_MODEL_REPOSITORY"
+MODEL_ANALYZER_BASE_ARGS="$MODEL_ANALYZER_BASE_ARGS --triton-http-endpoint localhost:$http_port --triton-grpc-endpoint localhost:$grpc_port"
+MODEL_ANALYZER_BASE_ARGS="$MODEL_ANALYZER_BASE_ARGS --triton-metrics-url http://localhost:$metrics_port/metrics"
+
+python3 test_config_generator.py --protocols "`echo $CLIENT_PROTOCOLS | sed 's/ /,/g'`" --launch-modes "`echo $TRITON_LAUNCH_MODES | sed 's/ /,/g'`"
+
+LIST_OF_CONFIG_FILES=(`ls | grep .yaml`)
+
+if [ ${#LIST_OF_CONFIG_FILES[@]} -le 0 ]; then
+    echo -e "\n***\n*** Test Failed. No config file exists. \n***"
+    exit 1
+fi
+
+RET=0
+
+function run_models() {
+    # SavedModel should be able to create a default config
+    PROFILE_MODEL="resnet_v1_50_cpu_savedmodel"
+    MA_EXPECTED_RESULT="EP"
+    run_server_launch_modes
+
+    # Libtorch model should NOT be able to create a default config
+    PROFILE_MODEL="vgg19_libtorch"
+    MA_EXPECTED_RESULT="EF"
+    run_server_launch_modes
+}
+
+function run_server_launch_modes() {
+    for CONFIG_FILE in ${LIST_OF_CONFIG_FILES[@]}; do
+        # e.g. config-docker-http.yaml -> config-docker-http
+        CONFIG_PARAMETERS=${CONFIG_FILE%".yaml"}
+
+        # config-docker-http -> [config, docker, http]
+        PARAMETERS=(${CONFIG_PARAMETERS//-/ })
+        LAUNCH_MODE=${PARAMETERS[1]}
+        PROTOCOL=${PARAMETERS[2]}
+        
+        ANALYZER_LOG=analyzer.${PROFILE_MODEL}.${LAUNCH_MODE}.${PROTOCOL}.log
+        SERVER_LOG=server.${PROFILE_MODEL}.${LAUNCH_MODE}.${PROTOCOL}.log
+
+        MODEL_ANALYZER_GLOBAL_OPTIONS="-v"
+        MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_BASE_ARGS -f $CONFIG_FILE --profile-models $PROFILE_MODEL"
+
+        _run_single_config
+
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    done
+}
+
+function _run_single_config() {
+    if [ "$LAUNCH_MODE" == "docker" ]; then
+        # Login to the GitLab CI. Required for pulling the Triton container.
+        docker login -u gitlab-ci-token -p "${CI_JOB_TOKEN}" "${CI_REGISTRY}"
+
+        MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_ARGS --triton-output-path=${SERVER_LOG} --triton-docker-image=$TRITON_SERVER_CONTAINER_IMAGE_NAME"
+    else
+        MODEL_ANALYZER_ARGS="$MODEL_ANALYZER_ARGS --triton-output-path=${SERVER_LOG}"
+    fi
+
+    _run_analyzer_and_check_results
+
+    if [ $? -ne 0 ]; then
+        return 1
+    fi        
+}
+
+function _run_analyzer_and_check_results() {
+    # Run the analyzer and check the results, enough to just profile the server
+    set +e
+    MODEL_ANALYZER_SUBCOMMAND="profile"
+    run_analyzer
+    MA_ACTUAL_RESULT=$?
+    _check_analyzer_exit_status
+
+    if [ ! -s "$SERVER_LOG" ]; then
+        echo -e "\n***\n*** Test Output Verification Failed : No logs found\n***"
+        cat $ANALYZER_LOG
+        RET=1
+        exit 1
+    fi
+
+    if [ $? -ne 0 ]; then
+        RET=1
+        return 1
+    fi
+    set -e
+
+    rm -rf checkpoints
+    rm -rf $OUTPUT_MODEL_REPOSITORY
+}
+
+function _check_analyzer_exit_status() {
+    if [ "$MA_EXPECTED_RESULT" == "EP" ]; then
+        # Expected Pass
+        if [ "$MA_ACTUAL_RESULT" != "0" ]; then
+            echo -e "\n***\n*** Test with launch mode '${LAUNCH_MODE}' using ${PROTOCOL} client Failed."\
+                    "\n***     model-analyzer exited with non-zero exit code (${MA_ACTUAL_RESULT}). \n***"
+            cat $ANALYZER_LOG
+            RET=1
+            exit 1
+        fi
+    elif [ "$MA_EXPECTED_RESULT" == "EF" ]; then
+        # Expected fail
+        if [ "$MA_ACTUAL_RESULT" == "0" ]; then
+            echo -e "\n***\n*** Test with launch mode '${LAUNCH_MODE}' using ${PROTOCOL} should have Failed."\
+                    "\n***     model-analyzer exited with zero exit code. (${MA_ACTUAL_RESULT})\n***"
+            cat $ANALYZER_LOG
+            RET=1
+            exit 1
+        fi
+    else
+        echo -e "\n***\n*** MA_EXPECTED_RESULT not setup properly. MA_EXPECTED_RESULT=${MA_EXPECTED_RESULT}\n***"
+        RET=1
+        exit 1
+    fi
+}
+
+
+run_models
+
+rm -rf $MODEL_REPOSITORY
+
+if [ $RET -eq 0 ]; then
+    echo -e "\n***\n*** Test PASSED\n***"
+else
+    echo -e "\n***\n*** Test FAILED\n***"
+fi
+
+exit $RET
---
+++ qa/L0_no_config_file/test_config_generator.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2021-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import yaml
+import os
+
+
+class TestConfigGenerator:
+    """
+    This class contains functions that
+    create configs for various test scenarios.
+    
+    The `setup` function does the work common to all tests
+
+    TO ADD A TEST: Simply add a member function whose name starts
+                    with 'generate'.
+    """
+
+    def __init__(self):
+        test_functions = [
+            self.__getattribute__(name)
+            for name in dir(self)
+            if name.startswith('generate')
+        ]
+
+        for test_function in test_functions:
+            self.setup()
+            test_function()
+
+    def setup(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '-p',
+            '--protocols',
+            type=str,
+            required=True,
+            help='Comma separated list of client protocols for this test')
+        parser.add_argument(
+            '-l',
+            '--launch-modes',
+            type=str,
+            required=True,
+            help='Comma separated list of launch modes for this test')
+
+        self.args = parser.parse_args()
+        self.protocols = sorted(self.args.protocols.split(','))
+        self.launch_modes = sorted(self.args.launch_modes.split(','))
+
+        self.config = {}
+        self.config['run_config_search_disable'] = True
+        self.config['batch_sizes'] = 4
+        self.config['concurrency'] = 4
+        self.config['perf_analyzer_cpu_util'] = 6000
+
+    def generate_configs(self):
+        for launch_mode in self.launch_modes:
+            self.config['triton_launch_mode'] = launch_mode
+            self.set_triton_server_path(launch_mode, self.config)
+            self.set_triton_install_path(launch_mode, self.config)
+
+            if launch_mode == 'c_api':
+                self.config['perf_output'] = True
+                with open(f'config-{launch_mode}-c_api.yaml', 'w') as f:
+                    yaml.dump(self.config, f)
+            else:
+                self.config['perf_output'] = False
+                for protocol in self.protocols:
+                    self.config['client_protocol'] = protocol
+                    if launch_mode == 'docker':
+                        # Set docker image and put in the CI runner's labels
+                        if 'TRITON_LAUNCH_DOCKER_IMAGE' in os.environ:
+                            self.config['triton_docker_image'] = os.environ[
+                                'TRITON_LAUNCH_DOCKER_IMAGE']
+                        if 'RUNNER_ID' in os.environ:
+                            self.config['triton_docker_labels'] = {
+                                'RUNNER_ID': os.environ['RUNNER_ID']
+                            }
+                    with open(f'config-{launch_mode}-{protocol}.yaml',
+                              'w') as f:
+                        yaml.dump(self.config, f)
+
+    def set_triton_server_path(self, launch_mode, config):
+        """
+        Helper function to set the 'triton_server_path' to something bogus
+            for all configuration except 'local'
+        This ensures only the 'local' launch mode uses 'triton_server_path'.
+        """
+        config.pop('triton_server_path', None)
+
+        if launch_mode != 'local':
+            config['triton_server_path'] = '/path/to/nowhere'
+
+    def set_triton_install_path(self, launch_mode, config):
+        """
+        Helper function to set the 'triton_install_path' to something bogus
+            for all configuration except 'c_api'
+        This ensures only the 'c_api' launch mode uses 'triton_install_path'.
+        """
+        config.pop('triton_install_path', None)
+
+        if launch_mode != 'c_api':
+            config['triton_install_path'] = '/path/to/nowhere'
+
+
+if __name__ == '__main__':
+    TestConfigGenerator()
