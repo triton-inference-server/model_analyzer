8925ca54c347e413757dd34577536025ff4dc04a
nv-hwoo <hwoo@nvidia.com>
2022-07-11 12:03:44 -0700

    Add script for sweeping all three model checkpoints

---
+++ experiments/start_profiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for model in inception_v1_graphdef resnet50_libtorch vgg19_libtorch; do
+  for radius in {2..8}; do
+    for magnitude in {1..8}; do
+      for min_initialized in {2..8}; do
+        echo "Profiling $model (radius = $radius, magnitude = $magnitude, min-initialized = $min_initialized)"
+        python3 main.py --save \
+            --model-name $model \
+            --generator UndirectedRunConfigGenerator \
+            --data-path /tmp/hwoo/datasets \
+            --output-path output \
+            --radius $radius \
+            --magnitude $magnitude \
+            --min-initialized $min_initialized \
+      done
+    done
+  done
+done
