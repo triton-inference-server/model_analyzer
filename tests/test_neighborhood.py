2d5afbb5d630c5b985a2e5e4429d0f716a807fa2
nv-hwoo <hwoo@nvidia.com>
2022-07-11 14:12:31 -0700

    Add test for all coordinates with zero throughputs

--- tests/test_neighborhood.py
+++ tests/test_neighborhood.py
@@ -263,3 +263,30 @@ class TestNeighborhood(trc.TestResultCollector):
         uv = n._get_step_vector()
         expected_uv = Coordinate([0, 0])
         self.assertEqual(uv, expected_uv)
+
+    def test_all_zero_throughputs(self):
+        """
+        Test that when all the coorindates in the neighborhood has
+        zero throughputs, the weighed coordinate is all 0s.
+        """
+        cd = CoordinateData()
+        cd.set_throughput(Coordinate([1, 0, 1]), 0)
+        cd.set_throughput(Coordinate([0, 1, 0]), 0)
+        cd.set_throughput(Coordinate([0, 1, 1]), 0)
+
+        dims = SearchDimensions()
+        dims.add_dimensions(0, [
+            SearchDimension("foo", SearchDimension.DIMENSION_TYPE_LINEAR),
+            SearchDimension("bar", SearchDimension.DIMENSION_TYPE_EXPONENTIAL),
+            SearchDimension("foobar", SearchDimension.DIMENSION_TYPE_EXPONENTIAL)
+
+        ])
+
+        nc = NeighborhoodConfig(dims, radius=3, min_initialized=3)
+
+        n = Neighborhood(nc, cd, Coordinate([0, 0, 0]))
+
+        coordinates, throughputs = n._compile_neighborhood_throughputs()
+        tc = n._determine_weighted_coordinate_center(coordinates, throughputs)
+        expected_tc = Coordinate([0, 0, 0])
+        self.assertEqual(tc, expected_tc)
