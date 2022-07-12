ca20db164775615197f54017b88f068185d4bc57
nv-hwoo <hwoo@nvidia.com>
2022-07-12 10:04:07 -0700

    Fix checkpoint directory

--- experiments/start_profiles.sh
+++ experiments/start_profiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CHECKPOINT_DIR="/mnt/nvdl/datasets/inferenceserver/model_analyzer_profile_results"
+
 for model in inception_v1_graphdef resnet50_libtorch vgg19_libtorch; do
   for radius in {2..8}; do
     for magnitude in {1..8}; do
@@ -8,7 +10,7 @@ for model in inception_v1_graphdef resnet50_libtorch vgg19_libtorch; do
         python3 main.py --save \
             --model-name $model \
             --generator UndirectedRunConfigGenerator \
-            --data-path /tmp/hwoo/datasets \
+            --data-path $CHECKPOINT_DIR \
             --output-path output \
             --radius $radius \
             --magnitude $magnitude \
