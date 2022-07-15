eb9d8cb1b2ac8b6503497d84c89bfd88be4b2012
tgerdes <tgerdes@nvidia.com>
2022-07-15 16:18:03 -0500

    more updates

--- qa/L0_type_checking/test.sh
+++ qa/L0_type_checking/test.sh
@@ -12,6 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-mypy --version
-mypy ../../model_analyzer/*.py --config-file ./mypi.ini
+mypy --config-file ./mypi.ini
 exit $?
