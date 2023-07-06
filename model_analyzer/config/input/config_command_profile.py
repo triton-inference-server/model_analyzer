bbeda0ffa4c40b8872cb56d2ed75bc9883263f24
Sai Kiran Polisetty <spolisetty@nvidia.com>
2023-07-06 17:42:42 +0530

    Revert "Fix warning about config class"
    This reverts commit 5150f4732bc431a89651d288507e804e50860b99.

--- model_analyzer/config/input/config_command_profile.py
+++ model_analyzer/config/input/config_command_profile.py
@@ -117,6 +117,7 @@ class ConfigCommandProfile(ConfigCommand):
     """
     def __init__(self):
         super().__init__()
+        self._fill_config()
 
     def _resolve_protobuf_field(self, field: FieldDescriptor) -> ConfigSweep:
         """
