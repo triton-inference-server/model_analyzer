63a5efba597206b7e50f151e67386723300e1577
Brian Raf <braf@nvidia.com>
2023-08-07 15:52:23 +0000

    Sorted results unit tests passing

--- model_analyzer/config/generate/manual_model_config_generator.py
+++ model_analyzer/config/generate/manual_model_config_generator.py
@@ -84,7 +84,7 @@ class ManualModelConfigGenerator(BaseModelConfigGenerator):
         # Indexed as follows:
         #    _configs[_curr_config_index][_curr_max_batch_size_index]
         #
-        self._configs = self._generate_model_configs()
+        self._configs = self._generate_model_config_variants()
 
     def _done_walking(self) -> bool:
         return len(self._configs) == self._curr_config_index
