c952c1ff1a53d6959cd4a4cfb6d9e903283ace18
Brian Raf <braf@nvidia.com>
2024-04-02 20:16:00 +0000

    Fixing codeQL issue

--- model_analyzer/perf_analyzer/genai_perf_config.py
+++ model_analyzer/perf_analyzer/genai_perf_config.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List
-
 from model_analyzer.model_analyzer_exceptions import TritonModelAnalyzerException
 
 
