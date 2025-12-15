e13fc1be0a19560fdbd1c844e4e48e69caaf1fbf
Misha Chornyi <mchornyi@nvidia.com>
2025-12-15 10:51:20 -0800

    Update copyright headers

--- model_analyzer/config/input/config_defaults.py
+++ model_analyzer/config/input/config_defaults.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 import os
